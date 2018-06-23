122e5907b18b4380883f11802160bf66a029bcfc
Felix Jen <fjen@felixjen.com>
2018-06-22 20:02:44 -0700

    added more documentation

--- Agent Scripts/mysql.sh
+++ Agent Scripts/mysql.sh
@@ -5,14 +5,14 @@
 # This script is used to backup the active MySQL Database to the Backup Server Copy specified in the variables. It works by taking a dump of the MySQL DB including all databases and sent directly to the backup server through an SSH connection after compression.
 
 # Set Variables. SET ALL VARIABLES BEFORE USE
-BACKUP_SERVER=""
-BACKUP_SERVER_USER=""
-BACKUP_SERVER_PATH=""
-SSH_PASSWORD=""
-MAILGUN_API_KEY=""
-ALERT_EMAIL=""
-DUMP_PASSWORD=""
-
+BACKUP_SERVER="" # IP address of the backup server 
+BACKUP_SERVER_USER="" # Username for the backup server login
+BACKUP_SERVER_PATH="" # Path at the destination, including the final folder name (/path/to/dir/server)
+SSH_PASSWORD="" # SSH Password because ssh keys do not work for this
+MAILGUN_API_KEY="" # API Key for Mailgun -> api:key-<THIS PART>
+ALERT_EMAIL="" # Email Address to receive backup alerts
+DUMP_PASSWORD="" # Password to encrypt the MySQL dump with
+SERVER_NAME="" # Canonical name for the server to be used in emails
 
 # Check to see if Backup Server is online. If not, exit the script because there is no point in running it.
 ping -q -c5 $BACKUP_SERVER > /dev/null
@@ -22,10 +22,10 @@ then
             https://api.mailgun.net/v3/mailgun.felixjen.com/messages \
             -F from='Ikora Alert <ikora@felixjen.com>' \
             -F to="$ALERT_EMAIL" \
-            -F subject='Ikora: MySQL Backup Failed' \
-            -F text='This is an emergency alert from Ikora. The MySQL scheduled backup has failed because the backup server is unreachable.'
+            -F subject="Ikora: $SERVER_NAME Backup Failed" \
+            -F text="This is an emergency alert from Ikora. The $SERVER_NAME scheduled backup has failed because the backup server is unreachable."
 	exit
 fi
 
 # Create the MySQL Database Dump
-mysqldump -u root -p$DUMP_PASSWORD --all-databases | sshpass -p "$SSH_PASSWORD" ssh $BACKUP_SERVER_USER@$BACKUP_SERVER "cat > $BACKUP_SERVER_PATH/mysql/mysqlserver.sql"
+mysqldump -u root -p$DUMP_PASSWORD --all-databases | sshpass -p "$SSH_PASSWORD" ssh $BACKUP_SERVER_USER@$BACKUP_SERVER "cat > $BACKUP_SERVER_PATH/mysqlserver.sql"
